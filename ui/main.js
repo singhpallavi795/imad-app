70989ab7f303dcffedeb152381233b09cef22380
singhpallavi795 <36466847+singhpallavi795@users.noreply.github.com>
2018-02-19 00:33:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,17 @@
 var button=document.getElementById('counter');
 var counter = 0;
 button.onclick = function () {
-    counter = counter+1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if(request.readyState == XMLHttpRequest.DONE) {
+            if(request.status == 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+                
+            }
+        }
+    };
+request.open('GET', 'http://singhpallavi795.imad.hasura.io/counter', true);
+request.send(null);
 };
